9d156efcd9d072a274dd86931b24063a9bcc5a27
luke <99561818+cvrme@users.noreply.github.com>
2023-09-26 00:14:02 -0400

    Add files via upload

---
+++ ManualObfKeyAuth/AuthorizationCheck.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BbbSkdfljsdlfjLDKJFLKSDJFLKSDJFk
+    {
+        private DddKsdlkfjlskdjfSDLKFJSKDLJFKJLSk _aAjsdklfjSldkjfsLKFJSKDJFSKLDFJKs;
+
+        public BbbSkdfljsdlfjLDKJFLKSDJFLKSDJFk()
+        {
+            _aAjsdklfjSldkjfsLKFJSKDJFSKLDFJKs = new DddKsdlkfjlskdjfSDLKFJSKDLJFKJLSk();
+        }
+
+        public bool CccSlkdfjlskdjfsLKDJFLKSJDLKFSJDl(string aAhsdkjflskdjlfsJKDFLSKJDLKFJSDKLs)
+        {
+            var bBksdlfjlsdkjfSKJLDFKJSLKDFJLKSDJf = DddDksdjflsdkjfLJKDFLJKSDLKJFSLKDFj(aAhsdkjflskdjlfsJKDFLSKJDLKFJSDKLs);
+            var cCsldkfjlskdfjLJKDFLKJSDLKJFLSDKJl = _aAjsdklfjSldkjfsLKFJSKDJFSKLDFJKs.EeeSldkfjlskdjfsLDKFJLKJSDFLKJSDFk(bBksdlfjlsdkjfSKJLDFKJSLKDFJLKSDJf);
+            return cCsldkfjlskdfjLJKDFLKJSDLKJFLSDKJl;
+        }
+
+        private string DddDksdjflsdkjfLJKDFLJKSDLKJFSLKDFj(string aAlsdkjflsdkjLSDKFJLSDKJFLKJSDFLj)
+        {
+            return aAlsdkjflsdkjLSDKFJLSDKJFLKJSDFLj.Replace("-", "").ToUpper();
+        }
+    }
+
+    public class DddKsdlkfjlskdjfSDLKFJSKDLJFKJLSk
+    {
+        private List<string> _aAksdljflsdkjfLSDKJFLKSDJFLKJSDFk;
+
+        public DddKsdlkfjlskdjfSDLKFJSKDLJFKJLSk()
+        {
+            FffSlkfjlskdjfSLDKFJLSDKJFLKJSDLKJf();
+        }
+
+        public bool EeeSldkfjlskdjfsLDKFJLKJSDFLKJSDFk(string aAksdlfjlskdjfSLKJDFLKJSDFLKJSDFLk)
+        {
+            var bBsdlkfjlskjdfLSDKJFLKSJDFLKJSDLFk = HhhKldfjlskdjfLKDJFLKJSDLKJFLKJSDLf.IiiSlkdjflsdkjflSDKJFLKJSDJFKJSDf(aAksdlfjlskdjfSLKJDFLKJSDFLKJSDFLk);
+            return _aAksdljflsdkjfLSDKJFLKSDJFLKJSDFk.Contains(bBsdlkfjlskjdfLSDKJFLKSJDFLKJSDLFk);
+        }
+
+        private void FffSlkfjlskdjfSLDKFJLSDKJFLKJSDLKJf()
+        {
+            _aAksdljflsdkjfLSDKJFLKSDJFLKJSDFk = new List<string>();
+            for (int i = 0; i < 100; i++)
+            {
+                _aAksdljflsdkjfLSDKJFLKSDJFLKJSDFk.Add(Guid.NewGuid().ToString().Replace("-", ""));
+            }
+        }
+    }
+
+    public static class HhhKldfjlskdjfLKDJFLKJSDLKJFLKJSDLf
+    {
+        public static string IiiSlkdjflsdkjflSDKJFLKJSDJFKJSDf(string aAlsdkjflskdjfLSDKJFLKSDJFLKJSDJFl)
+        {
+            return aAlsdkjflskdjfLSDKJFLKSDJFLKJSDJFl.Reverse().ToString();
+        }
+    }
+
+    public class JjjSldkjfslkdjfsLDKFJLKJSDLKFJLKJSd
+    {
+        public string KkkSlkdjfslkdfjSLKJDFLKJSDJFKJSDLFj()
+        {
+            var aAlsdfjlskdjflSDKFJLKSDJFKLJSDLKFj = Guid.NewGuid().ToString().Replace("-", "");
+            var bBsldkfjlskdjfSLKJDFLKJSDFLKJLSDKFj = LllSlkdjfslkdfjSDLKFJLKJDFLKJDFLKJl.MmmSlkdjfslkdfjLSDKFJLKJSDFLKJDSLKf(aAlsdfjlskdjflSDKFJLKSDJFKLJSDLKFj);
+            return bBsldkfjlskdjfSLKJDFLKJSDFLKJLSDKFj;
+        }
+    }
+
+    public static class LllSlkdjfslkdfjSDLKFJLKJDFLKJDFLKJl
+    {
+        public static string MmmSlkdjfslkdfjLSDKFJLKJSDFLKJDSLKf(string aAlskdjflskdjfLSDKFJLSDKJFLKJSDKLf)
+        {
+            return new string(aAlskdjflskdjfLSDKFJLSDKJFLKJSDKLf.Reverse().ToArray());
+        }
+    }
+
+    public static class NnnSlkdjflskdjflSDKFJLKJSDFLKJDFLKJl
+    {
+        public static bool OooSlkfjlsdkjfLSDKJFLKJSDFLKJDFLKJd(string aAlskdjflsdjflSDKJFLKJSDFLKJDFLKJl)
+        {
+            return aAlskdjflsdjflSDKJFLKJSDFLKJDFLKJl.Length == 32 && aAlskdjflsdjflSDKJFLKJSDFLKJDFLKJl.All(char.IsUpper);
+        }
+    }
